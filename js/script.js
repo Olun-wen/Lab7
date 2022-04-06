55842cecb8a1916a7009061310274988b3244ad0
Olun-wen <82757905+Olun-wen@users.noreply.github.com>
2022-04-06 08:11:18 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function init(){
   //add your javascrip between these two lines of code
   function alertFunction(){
-   var text = document.getElementById("entryinput").value;
-   alert("Yulun Wen: " + text);
-   document.getElementById("textoutput").innerHTML = text;
+     var text = document.getElementById("entryinput").value;
+     alert("Yulun Wen: " + text);
+     document.getElementById("textoutput").innerHTML = text;
   }
  
   var entrybtn = document.getElementById("entrybutton");
